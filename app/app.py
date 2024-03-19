8d98b0e50cf9eaadde9ba5e4960d9f2f94d9f1a8
Yuto Chikazawa <chikazawa0517@gmail.com>
2024-03-20 02:58:14 +0900

    Fix driver_type in workflow

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-import os
+import argparse
 from logging import DEBUG, INFO
 
 from logger_manager import get_logger
@@ -8,11 +8,23 @@ from driver_context import DriverContext
 logger = get_logger(__name__, INFO)
 
 
-with DriverContext('remote') as driver:
 
-    driver.get("https://www.google.com")
+def parse_args():
+    parser = argparse.ArgumentParser(description='Auto reservator command line arguments.')
+    parser.add_argument('--driver_type', "-d", type=str, default='remote', help='web driver which selenium used. \'remote\' or \'chrome\'')
+    args = parser.parse_args()
+    return args
 
-    logger.info(driver.title)
-    screenshot_path = f"log/{get_current_datetime_str()}_{driver.title}.png"
-    driver.save_screenshot(screenshot_path)
+
+if __name__ == '__main__':
+    # Parse arguments.
+    parse_args = parse_args()
+        
+    with DriverContext(parse_args.driver_type) as driver:
+
+        driver.get("https://www.google.com")
+
+        logger.info(driver.title)
+        screenshot_path = f"log/{get_current_datetime_str()}_{driver.title}.png"
+        driver.save_screenshot(screenshot_path)
 

311c0f24ba5e994e929f3c62124e95c1344a351b
Yuto Chikazawa <chikazawa0517@gmail.com>
2024-03-20 02:51:57 +0900

    Add driver context

--- app/app.py
+++ app/app.py
@@ -1,26 +1,18 @@
 import os
-from selenium import webdriver
 from logging import DEBUG, INFO
-import chromedriver_binary  # Adds chromedriver binary to path
 
 from logger_manager import get_logger
 from utils import get_current_datetime_str
+from driver_context import DriverContext
 
 logger = get_logger(__name__, INFO)
 
 
-options = webdriver.ChromeOptions()
-options.add_argument('--no-sandbox')
-options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Remote(
-    command_executor = os.environ['SELENIUM_URL'],
-    options = options,
-)
+with DriverContext('remote') as driver:
 
-driver.get("https://www.google.com")
+    driver.get("https://www.google.com")
 
-logger.info(driver.title)
-screenshot_path = f"log/{get_current_datetime_str()}_{driver.title}.png"
-driver.save_screenshot(screenshot_path)
+    logger.info(driver.title)
+    screenshot_path = f"log/{get_current_datetime_str()}_{driver.title}.png"
+    driver.save_screenshot(screenshot_path)
 
-driver.quit()
---
+++ app/driver_context.py
@@ -0,0 +1,31 @@
+import os
+import time
+from selenium import webdriver
+
+class DriverContext:
+    def __init__(self, driver_type='remote'):
+        # Create webdriver
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument("--disable-dev-shm-usage")
+
+        if driver_type == 'remote':
+            self.driver = webdriver.Remote(
+                command_executor = os.environ['SELENIUM_URL'],
+                options = options,
+            )
+        elif driver_type == 'chrome':
+            options.add_argument('--headless')
+            # impersonate webdriver
+            options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+            self.driver = webdriver.Chrome(options=options)
+            # impersonate webdriver
+            self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+
+    def __enter__(self):
+        return self.driver
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        time.sleep(3)
+        self.driver.quit()
